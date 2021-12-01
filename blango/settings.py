bb707f771ef507ad04fb6a9818b9b275b9e00b83
Satish Iyer <satish.iyer@landisgyr.com>
2021-12-01 02:56:51 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['exodus-join-8000.codio.io']
+ALLOWED_HOSTS = ['turbo-copper-8000.codio.io']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'blog',
+     'rest_framework'
 ]
 
 MIDDLEWARE = [
