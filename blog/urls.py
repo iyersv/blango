aa4d2818e6a7cca282d6ff76e0d6e05c3d5abb92
Satish Iyer <satish.iyer@landisgyr.com>
2021-12-01 00:11:35 +0000

    Finish first-party django api

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-# other imports
-from blog.views import index
 from django.urls import path
 
+from blog.api_views import post_list, post_detail
+
 urlpatterns = [
-    path("", index)
+    path("posts/", post_list, name="api_post_list"),
+    path("posts/<int:pk>/", post_detail, name="api_post_detail"),
 ]
